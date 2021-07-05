4bdafdf3b34c110b1f9ff22988950d371ff80706
Jordan <jordankbartos@gmail.com>
2021-07-04 18:08:57 -0700

    add git configuration

--- configure.sh
+++ configure.sh
@@ -1,3 +1,13 @@
+echo "Copying vimrc --> ~/.vimrc..."
 cp ./vimrc ~/.vimrc
+echo $?
+
+echo "Copying bashrc --> ~/.bashrc..."
 cp ./bashrc ~/.bashrc
+echo $?
+
+echo "configure git..."
+git config --global pull.ff only
+echo $?
+
 bash
